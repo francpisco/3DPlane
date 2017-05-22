8b59d6ec1f6e70dedef39ca98236c26841a6a6ee
francpisco <fmpapt@gmail.com>
2017-05-22 10:42:17 +0100

    First commit

---
+++ src/pt/altran/roothless/model/PlaneDyn.java
@@ -0,0 +1,19 @@
+package pt.altran.roothless.model;
+
+/**
+ * Created by Altran on 22/05/2017.
+ */
+public class PlaneDyn {
+
+    private double xPosition;
+    private double yPosition;
+    private double zPosition;
+
+    private double xVelocity;
+    private double yVelocity;
+    private double zVelocity;
+
+
+
+
+}
